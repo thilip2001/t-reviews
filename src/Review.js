aa74b46177b5b22b63262a3551a12d9e45fe2998
Thiliban <thilip2017@gmail.com>
2022-11-07 15:37:25 +0530

    random buttons upadted

--- src/Review.js
+++ src/Review.js
@@ -29,6 +29,15 @@ const Review = () => {
     });
   };
 
+  const randomPerson = () =>{
+      // setIndex((Math.floor(Math.random()*people.length)))
+      let randomNumber = Math.floor(Math.random()*people.length);
+      if(randomNumber===index){
+        randomNumber = index+1
+      }
+      setIndex(checkNumber(randomNumber))
+  }
+
   return (
     <article className="review">
       <div className="img-container">
@@ -48,7 +57,7 @@ const Review = () => {
           <FaChevronRight />
         </button>
       </div>
-      <button className="random-btn">Suprise me</button>
+      <button className="random-btn" onClick={randomPerson}>Suprise me</button>
     </article>
   );
 };
