09746a777bc1e71444f446b0af114e03d3a9abc1
Thiliban <thilip2017@gmail.com>
2022-11-07 11:39:10 +0530

    updated

--- src/App.js
+++ src/App.js
@@ -1,13 +1,15 @@
 import React from "react";
+import Review from "./Review";
 function App() {
   return (
     <main>
-      <div className="container">
+      <section className="container">
         <div className="title">
           <h2>Our Reviews</h2>
           <div className="underline"></div>
         </div>
-      </div>
+        <Review/>
+      </section>
     </main>
   );
 }
